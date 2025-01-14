a50f4fa2fa046b8dfeaf86b477ad6b0c0019ab48
knpoblete <pobletekath@gmail.com>
2025-01-14 21:53:00 +0000

    change publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/knpoblete.github.io/'
+  publicPath: 'https://knpoblete.github.io/'
 })
 
