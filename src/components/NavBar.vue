1e575fb73606fbf286c848b514b1815f9f563f06
knpoblete <pobletekath@gmail.com>
2025-02-07 14:22:12 +0000

    update nav bar for md size screen

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,19 +17,19 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-[#3F4739] rounded-lg bg-[#3F4739] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#3F4739] dark:bg-[#3F4739] md:dark:bg-[#3F4739] dark:border-gray-700">
             <li>
-                <a href="#about-section" class="block py-2 px-3 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]" aria-current="page">About</a>
+                <a href="#about-section" class="block py-2 px-3 text-white hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]" aria-current="page">About</a>
             </li>
             <li>
-                <a href="#skills-section" class="block py-2 px-3 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Skills</a>
+                <a href="#skills-section" class="block py-2 px-3 text-white hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Skills</a>
             </li>
             <li>
-                <a href="#projects-section" class="block py-2 px-3 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Projects</a>
+                <a href="#projects-section" class="block py-2 px-3 text-white hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Projects</a>
             </li>
             <li>
-                <a href="#testimonials-section" class="block py-2 px-3 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Testimonials</a>
+                <a href="#testimonials-section" class="block py-2 px-3 text-white hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Testimonials</a>
             </li>
             <li>
-                <a href="#contact-section" class="block py-2 px-3 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Contact</a>
+                <a href="#contact-section" class="block py-2 px-3 text-white hover:bg-gray-100 dark:text-white dark:hover:bg-[#3F4739] dark:hover:text-[#F1BF98] dark:border-[#3F4739]">Contact</a>
             </li>
             </ul>
         </div>
